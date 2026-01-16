60fe17e26b6813b44d82c8d2e440320fabcf46a5
Jampa Matos <jp.coutm@gmail.com>
2026-01-16 17:11:08 -0300

    feat: connect postgres via DATABASE_URL

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from dotenv import load_dotenv
 from pathlib import Path
+import dj_database_url
 import os
 
 # Load dotenv
@@ -79,10 +80,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=60,
+    )
 }
 
 
