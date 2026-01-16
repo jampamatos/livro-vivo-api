cd691ede56ae1e887bd054ea1673cd4e82b8775b
Jampa Matos <jp.coutm@gmail.com>
2026-01-16 17:17:51 -0300

    feat: add health endpoint

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
+
+def health(_request):
+    return JsonResponse({'status': 'ok'})
 
 urlpatterns = [
+    path('health/', health),
     path('admin/', admin.site.urls),
 ]
