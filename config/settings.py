00cc8a2422764fd8499a14c8162ef6a765218b9a
Jampa Matos <jp.coutm@gmail.com>
2026-01-20 12:54:15 -0300

    feat: add CORS support by including django-cors-headers in settings

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
     
     'rest_framework',
     'rest_framework.authtoken',
@@ -55,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,3 +155,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8081',
+    'http://127.0.0.1:8081',
+    # bônus (às vezes Expo usa 19006 em alguns setups)
+    'http://localhost:19006',
+    'http://127.0.0.1:19006',
+]
