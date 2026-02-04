6db666e552d8250725a0a94b4e368e851f557422
Jampa Matos <jp.coutm@gmail.com>
2026-02-04 10:36:14 -0300

    feat(caselaw): register CaseLaw in admin with filters and search

--- caselaw/admin.py
+++ caselaw/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CaseLaw
+
+@admin.register(CaseLaw)
+class CaseLawAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'court',
+        'case_number',
+        'decision_date',
+        'relevance',
+        'updated_at',
+    )
+    list_filter = (
+        'court',
+        'decision_date',
+        'relevance',
+    )
+    search_fields = (
+        'court',
+        'case_number',
+        'summary',
+    )
+    ordering = ('-decision_date', '-updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'court',
+                'case_number',
+                'decision_date',
+                'url',
+                'relevance',
+                'tags',
+            )
+        }),
+        ('Conteúdo', {
+            'fields': ('summary',),
+        }),
+        ('Metadados', {
+            'fields': ('created_at', 'updated_at'),
+        }),
+    )
